a1f70cb51be722873242a670e2810a963b9dbfee
swarnayuroy <swarnayuroy@gmail.com>
2024-09-16 23:34:02 +0530

    added DTO UserTask

---
+++ microservice/API_Service/DTO/UserTask.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using API_Service.Models;
+
+namespace API_Service.DTO
+{
+    public class UserTask
+    {
+        public User User { get; set; }
+        public IList<Task> Tasks { get; set; }
+        public IList<TaskComment> Comments { get; set; }
+    }
+}
