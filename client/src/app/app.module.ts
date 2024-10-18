817bbd44ee324860fea9eeebfe372eb0a7d5074e
BANTUR-LAPPY\Administrator <sianish5@gmail.com>
2024-10-18 23:07:10 +0530

    created signin and signup component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserSigninComponent } from './user-signin/user-signin.component';
+import { UserSignupComponent } from './user-signin/user-signup/user-signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserSigninComponent,
+    UserSignupComponent
   ],
   imports: [
     BrowserModule,
---
+++ client/src/app/user-signin/user-signin.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-signin',
+  templateUrl: './user-signin.component.html',
+  styleUrls: ['./user-signin.component.scss']
+})
+export class UserSigninComponent {
+
+}
